5a5f17e4e6d23a00dad69a5ba1bc5f5912c72e73
Yichao Xue <violethaze871007@gmail.com>
2014-04-09 04:43:11 -0400

    modify entry.java

--- src/mapred/main/Entry.java
+++ src/mapred/main/Entry.java
@@ -1,13 +1,10 @@
 package mapred.main;
 
-import mapred.util.SimpleParser;
 
 public class Entry {
 	public static void main(String args[]) throws Exception  {
-		SimpleParser parser = new SimpleParser(args);
-		//String program = parser.get("program");
 		
-		//System.out.println("Running program " + program + "..");
+		System.out.println("Starting......");
 
 		long start = System.currentTimeMillis();
 
